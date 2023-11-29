82e5a93df04e8c7a663b9171f2a9510b67d912be
Alice Bedow <alice.bedow@gmail.com>
2023-11-29 17:04:22 +0100

    Spring Microservices - Concurrency-Exchange und CurrencyConversion Service

--- currency-conversion-service/src/main/java/de/alice/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/de/alice/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package de.alice.currencyconversionservice.controller;
 
 import de.alice.currencyconversionservice.bean.CurrencyConversion;
+import de.alice.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,14 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    private final CurrencyExchangeProxy currencyExchangeProxy;
+
+    @Autowired
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         // Create HashMap with the Parameters of the Webservice to the currency-exchange microservice
@@ -25,4 +35,13 @@ public class CurrencyConversionController {
 
         return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
     }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        // Call currency-exchange microservice using Feign
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+    }
+
 }
---
+++ currency-conversion-service/src/main/java/de/alice/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package de.alice.currencyconversionservice.proxy;
+
+import de.alice.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Proxy for the currency-exchange microservice
+ */
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
