8fefe847c537a2d38d03b56038252759bec58f82
Alice Bedow <alice.bedow@gmail.com>
2023-12-27 18:23:18 +0100

    Spring Microservices - ZipKin

--- currency-conversion-service/src/main/java/de/alice/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/de/alice/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,9 @@ package de.alice.currencyconversionservice.controller;
 import de.alice.currencyconversionservice.bean.CurrencyConversion;
 import de.alice.currencyconversionservice.proxy.CurrencyExchangeProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,14 +15,25 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
 
     private final CurrencyExchangeProxy currencyExchangeProxy;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy, RestTemplate restTemplate) {
         this.currencyExchangeProxy = currencyExchangeProxy;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
@@ -30,7 +44,7 @@ public class CurrencyConversionController {
         uriVariables.put("to", to);
 
         // Call currency-exchange microservice
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
         CurrencyConversion currencyConversion = responseEntity.getBody();
 
         return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
